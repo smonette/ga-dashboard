f57479d205fca9c64679204d2e479cd1b24c0c68
scienceoflife <austin.west@gmail.com>
2014-09-02 16:01:58 -0700

    ajax arduino connection

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,5 +1,7 @@
 class SitesController < ApplicationController
-  before_action :nokogiri_soups, :nokogiri_trucks
+
+respond_to :html, :json
+  # before_action :nokogiri_soups, :nokogiri_trucks
   def index
     @user = User.new
 
@@ -11,46 +13,48 @@ class SitesController < ApplicationController
 
   end
   def show
-
+    @button1 = @@button1
+    respond_with @button1
   end
+  
 
-  def nokogiri_trucks
-    urlTrucks = open('http://www.gfoodlounge.com/truckschedule/').read
+#   def nokogiri_trucks
+#     urlTrucks = open('http://www.gfoodlounge.com/truckschedule/').read
 
-    # Nokogiri is the module name, HTML is the class name
-    pageTrucks = Nokogiri::HTML(urlTrucks)
-    @trucks = []
+#     # Nokogiri is the module name, HTML is the class name
+#     pageTrucks = Nokogiri::HTML(urlTrucks)
+#     @trucks = []
 
-    @trucks = pageTrucks.css('.otg-vendor-data a').map do |truck|
-      {title: truck.text, url: truck["href"]}
-    end
-    p @trucks
-  end
+#     @trucks = pageTrucks.css('.otg-vendor-data a').map do |truck|
+#       {title: truck.text, url: truck["href"]}
+#     end
+#     p @trucks
+#   end
 
-  def nokogiri_soups
-    urlSoups = open('http://www.specialtys.com/Location.aspx?Store=SF09#TodaysSoups').read
+#   def nokogiri_soups
+#     urlSoups = open('http://www.specialtys.com/Location.aspx?Store=SF09#TodaysSoups').read
 
-    # Nokogiri is the module name, HTML is the class name
-    pageSoups = Nokogiri::HTML(urlSoups)
-    @soups = []
+#     # Nokogiri is the module name, HTML is the class name
+#     pageSoups = Nokogiri::HTML(urlSoups)
+#     @soups = []
 
-    @soups = pageSoups.css('a.soupSelection').map do |soup|
-      # @images = pageSoups.css('a.soupSelection img').each do |img|
-      #   img['src']
-      # end
-     {name: soup.text.strip, img: @images }
-    end
-  end
+#     @soups = pageSoups.css('a.soupSelection').map do |soup|
+#       # @images = pageSoups.css('a.soupSelection img').each do |img|
+#       #   img['src']
+#       # end
+#      {name: soup.text.strip, img: @images }
+#     end
+#   end
 
-  def nokogiri_ga
-    urlGA = open('https://generalassemb.ly/education?where=san-francisco&format=classes-workshops').read
+#   def nokogiri_ga
+#     urlGA = open('https://generalassemb.ly/education?where=san-francisco&format=classes-workshops').read
 
-    # Nokogiri is the module name, HTML is the class name
-    pageGA = Nokogiri::HTML(urlGA)
-    @ga = []
+#     # Nokogiri is the module name, HTML is the class name
+#     pageGA = Nokogiri::HTML(urlGA)
+#     @ga = []
 
-    @ga = pageGA.css('.title a').map do |link|
-      {title: link.text, url: link["href"]}
-    end
-  end
+#     @ga = pageGA.css('.title a').map do |link|
+#       {title: link.text, url: link["href"]}
+#     end
+#   end
 end
