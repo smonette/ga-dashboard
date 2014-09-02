ebe60a0dd00b1af4aa1d23565b9593fc0b8fdae7
smonette <steph@stephmonette.com>
2014-09-02 11:00:39 -0700

    adding the soup tile

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,4 +1,5 @@
 class SitesController < ApplicationController
+  before_action :nokogiri_soups
   def index
     @user = User.new
 
@@ -8,21 +9,44 @@ class SitesController < ApplicationController
 
     @current_user = User.find_by_id(session[:id])
 
-    # puts "!!!!!!!!!!!!!!#{@current_user}!!!!!!!!!!!!!!!!!"
   end
   def show
 
   end
-  def nokogiri
-    url = open('http://www.gfoodlounge.com/truckschedule/').read
+  def nokogiri_trucks
+    urlTrucks = open('http://www.gfoodlounge.com/truckschedule/').read
 
     # Nokogiri is the module name, HTML is the class name
-    page = Nokogiri::HTML(url)
-    results = []
+    pageTrucks = Nokogiri::HTML(urlTrucks)
+    @trucks = []
 
-    results = page.css('.title a').map do |link|
+    @trucks = pageTrucks.css('.title a').map do |link|
       {title: link.text, url: link["href"]}
     end
+  end
+  def nokogiri_soups
+    urlSoups = open('http://www.specialtys.com/Location.aspx?Store=SF09#TodaysSoups').read
 
+    # Nokogiri is the module name, HTML is the class name
+    pageSoups = Nokogiri::HTML(urlSoups)
+    @soups = []
+
+    @soups = pageSoups.css('a.soupSelection').map do |soup|
+      # @images = pageSoups.css('a.soupSelection img').each do |img|
+      #   img['src']
+      # end
+     {name: soup.text.strip, img: @images }
+    end
+  end
+  def nokogiri_ga
+    urlGA = open('https://generalassemb.ly/education?where=san-francisco&format=classes-workshops').read
+
+    # Nokogiri is the module name, HTML is the class name
+    pageGA = Nokogiri::HTML(urlGA)
+    @ga = []
+
+    @ga = pageGA.css('.title a').map do |link|
+      {title: link.text, url: link["href"]}
+    end
   end
 end
