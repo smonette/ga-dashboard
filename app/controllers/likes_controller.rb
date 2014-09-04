68aa8343a71d623247206d60b901174bac497ec0
Adam Budik <asbudik@live.com>
2014-09-03 18:39:02 -0700

    ajax likes implemented

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,13 @@
 class LikesController < ApplicationController
+  # def create
+  #   # binding.pry
+  #   # render json: params
+
+
+  # end
   def create
     @user = User.find_by_id(session[:id])
-    @shoutout = Shoutout.find_by_id(params[:parent_id])
+    @shoutout = Shoutout.find_by_id(params["parent_id"])
 
     all_shoutout_likes = @shoutout.likes
 
@@ -16,12 +22,21 @@ class LikesController < ApplicationController
     if unique == true
       last_like = @shoutout.likes.order(created_at: :asc).last
       new_like = last_like["likes"] += 1
-      @create_like = Like.create(likes: new_like, user_likes_id: @user.id)
+      @create_like = Like.create(likes: new_like, :user_likes_id => @user.id)
 
-      @shoutout.likes << @create_like
-      redirect_to '/'
+      @update_like = @shoutout.likes << @create_like
+      render :json => new_like
+      # redirect_to '/'
     else
-      redirect_to '/'
+      # all_shoutout_likes.each do |compare_like|
+      #   if compare_like.user_likes_id == @user.id
+      #     compare_like.destroy
+      #   end
+      # last_like = @shoutout.likes.order(created_at: :asc).last
+      # new_like = last_like["likes"] -= 1
+      # @create_like = Like.create(likes: new_like)
+      # last_like.destroy
+      # @shoutout.likes << @create_like
     end
   end
 end
