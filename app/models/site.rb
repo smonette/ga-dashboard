6aa55fed521c476b06429da8f508098a624ee65d
smonette <steph@stephmonette.com>
2014-09-04 13:17:25 -0700

    ugh workshop

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,29 +1,29 @@
 class Site < ActiveRecord::Base
 
-  def self.do_something
-    Workshop.destroy_all
-    urlGA = open('https://generalassemb.ly/education?where=san-francisco&format=classes-workshops').read
-    pageGA = Nokogiri::HTML(urlGA)
+  # def self.do_something
+  #   # Workshop.destroy_all
+  #   urlGA = open('https://generalassemb.ly/education?where=san-francisco&format=classes-workshops').read
+  #   pageGA = Nokogiri::HTML(urlGA)
 
-    scripts = pageGA.css('script')
-    a = scripts.select {|s| s.text.include? "EDUCATIONAL_OFFERINGS_JSON"}.first.content.split("\n")
-
-
-    # binding.pry
-    @parsedScript = JSON.parse(a[1].match(/\[[^;]*/).to_s)
-    @parseCourses = @parsedScript.sort_by{|course| course["starts"] }
-    @gaCourses = @parseCourses.take(20)
+  #   scripts = pageGA.css('script')
+  #   a = scripts.select {|s| s.text.include? "EDUCATIONAL_OFFERINGS_JSON"}.first.content.split("\n")
 
 
-    @gaCourses.each do |course|
-      url = course["url"]
-      regex = /\.ly(.*)\z/i
+  #   # binding.pry
+  #   @parsedScript = JSON.parse(a[1].match(/\[[^;]*/).to_s)
+  #   @parseCourses = @parsedScript.sort_by{|course| course["starts"] }
+  #   @gaCourses = @parseCourses.take(20)
 
-      if matched_url = regex.match(url)
-        url = matched_url[1]
-      end
 
-      Workshop.create(url: url, title: course["title"], date: course["date_description"])
-    end
-  end
+  #   @gaCourses.each do |course|
+  #     url = course["url"]
+  #     regex = /\.ly(.*)\z/i
+
+  #     if matched_url = regex.match(url)
+  #       url = matched_url[1]
+  #     end
+
+  #     # Workshop.create(url: url, title: course["title"], date: course["date_description"])
+  #   end
+  # end
 end
